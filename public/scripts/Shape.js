93db2dac01382631b6cbb4a47166ed6f7ee63c1b
adam01140 <achaaban@ucsc.edu>
2024-02-13 21:31:54 -0800

    q

--- public/scripts/Shape.js
+++ public/scripts/Shape.js
@@ -14,7 +14,10 @@ let myRandomList = [2, 2, 0, 1, 1, 0, 2, 2, 0, 1, 2, 1, 1, 1, 2, 0, 0, 2, 2, 0];
 
 
 
-
+socket.emit('requestRandomList');
+socket.on('receiveRandomList', (receivedList) => {
+	myRandomList = receivedList;
+});
 
 
 

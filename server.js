187ff2f8561e8e0b381790f5a95348508b1420f6
adam01140 <achaaban@ucsc.edu>
2024-02-13 19:59:21 -0800

    q

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const io = socketIo(server, {
 // Serve a simple HTTP response for root path
 app.get('/', (req, res) => {
     res.send('Server is running.');
-
+});
 
 
 
@@ -123,5 +123,3 @@ server.listen(PORT, () => {
 });
 
 console.log(`ello`);
-
-});

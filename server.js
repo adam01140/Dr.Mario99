42816c900734b72d52e4c335e6cc074054d56a8f
adam01140 <achaaban@ucsc.edu>
2024-02-13 18:54:49 -0800

    w

--- server.js
+++ server.js
@@ -43,8 +43,6 @@ console.log(randomList);
 const playersInLobby = { count: 0 };
 
 
-
-
 const activeRooms = {};
 
 io.on('connection', (socket) => {
@@ -108,7 +106,7 @@ io.on('connection', (socket) => {
 	
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });

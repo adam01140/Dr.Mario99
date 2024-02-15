2a3a73a7144c91aedf68b756cb62501ed8613d70
adam01140 <achaaban@ucsc.edu>
2024-02-14 22:29:53 -0800

    spawns dot

--- public/scripts/Board.js
+++ public/scripts/Board.js
@@ -663,7 +663,6 @@ class ThrowingBoard extends Board {
 		alert('x = ' + x + 'y = ' + y);
         // Check if the position is not already taken.
         if (!this.fields[x][y].isTaken()) {
-			alert('x = ' + x + 'y = ' + y);
             this.fields[x][y].setColor(color); // Set the color of the field to yellow.
             // Additional logic to mark the field as taken or to associate it with a shape, if necessary.
         }
